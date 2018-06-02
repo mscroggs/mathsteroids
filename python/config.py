201d8453052670189b595067183b69c059165aeb
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2018-06-02 15:10:33 +0100

    ..

--- python/config.py
+++ python/config.py
@@ -2,5 +2,5 @@ import os as _os
 
 path = _os.path.dirname(_os.path.realpath(__file__))
 
-with open(_os.path.join(path, "VERSION")) as f:
+with open(_os.path.join(path, "../VERSION")) as f:
     VERSION = f.read().strip("\n")
